2c6c82bfc77894279e4b7241822e97a92583995c
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-09-23 10:54:17 +0500

    Project Installation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import { Button, Icon } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col justify-center items-center gap-5'>
+      <h1 className="text-3xl font-bold underline text-green-400 mt-5">
+        Hello world!
+      </h1>
+      <Button variant="contained" color='success'>Get Started</Button>
+      <Icon>star</Icon>
     </div>
   );
 }
