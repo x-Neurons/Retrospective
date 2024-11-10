b23e064c17f6ad450d1858c908be4bb25850e1eb
Shahryar-Sohail <shahryar.ch123@gmail.com>
2024-11-10 17:55:50 +0500

    Uploading Project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import { Button, Icon } from '@mui/material';
 import './App.css';
-import BrainStorm from './Components/BrainStorm';
 import Group from './Components/Group';
 
 function App() {
-  const currentPath = window.location.pathname; // Get the current path
-
-  return (
-    <>
-      {currentPath === '/group' ? <Group /> : <BrainStorm />}
-    </>
-  );
+  return <Group />; // Directly render the Group component as the default
 }
 
 export default App;
--- src/Shared/data.js
+++ src/Shared/data.js
@@ -1,4 +1,3 @@
-
 const data = [
     { 
       id: 1, 
